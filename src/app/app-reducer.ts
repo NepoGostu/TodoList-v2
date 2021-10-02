c8e4624f25d2b9a439ed49790757de66487ada9a
NepoGostu <andruavanshuls@gmail.com>
2021-10-02 19:45:54 +0300

    14 lsn Added ThunkCreators

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,38 @@
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+//status === 'loading '
+// status === 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: 'SUPER ERROR' as string | null
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-ERROR' :
+            return {...state, error: action.error}
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        default:
+            return state
+    }
+}
+
+export const  setAppStatusAC = (status: RequestStatusType) => {
+    return {
+        type: 'APP/SET-STATUS',
+        status
+    } as const
+}
+
+export const setErrorAC = (error: string | null) => ({type:'APP/SET-ERROR', error } as const)
+
+export type setErrorActionType = ReturnType<typeof setErrorAC>
+
+type AppStatusType = ReturnType<typeof setAppStatusAC>
+
+type ActionsType = AppStatusType | setErrorActionType
